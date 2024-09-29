0fd04c5c4c92dab628b1a99f748a80f1fa25c63f
DEEPAK P <deepakram1774@gmail.com>
2024-09-29 19:55:28 +0530

    Add files via upload

---
+++ DAY 3/PassChecker.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class PassChecker {
+    public static void main(String[] args) {
+        
+        final String CORRECT_PASSWORD = "secret123";
+
+       
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            
+            System.out.print("Enter password: ");
+            String enteredPassword = scanner.nextLine();
+
+           
+            if (enteredPassword.equals(CORRECT_PASSWORD)) {
+                
+                System.out.println("Password accepted.");
+                break;
+            } else {
+                
+                System.out.println("Incorrect password. Try again.");
+            }
+        }
+       
+        scanner.close();
+    }
+}
